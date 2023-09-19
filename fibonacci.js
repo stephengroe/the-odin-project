bbcf8dc05383da89290a3bd58161b60f3137ca55
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-09-18 17:22:33 -0700

    Fix: off-by-one error

--- fibonacci.js
+++ fibonacci.js
@@ -1,13 +1,16 @@
 function getFibonacci(num) {
-  if (num < 0) {return "Error: negative number!";}
-  if (num <= 1) { return [0, 1];}
+  if (num < 0) { return "Error: Invalid number!"; }
+  if (num <= 2) { return [0, 1]; }
+
+  debugger
   
-  const resultArray = getFibonacci(num - 1);
-  resultArray.push(resultArray[resultArray.length - 1] + resultArray[resultArray.length - 2])
+  let resultArray = getFibonacci(num - 1);
+  resultArray.push(resultArray[resultArray.length - 1] + resultArray[resultArray.length - 2]);
+
   return resultArray;
 }
 
-console.log(getFibonacci(-5)); // Error: negative number!
-console.log(getFibonacci(1)); // [0]
+console.log(getFibonacci(-5)); // Error: Invalid number!
+console.log(getFibonacci(2)); // [0, 1]
 console.log(getFibonacci(5)); // [0, 1, 1, 2, 3]
 console.log(getFibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
