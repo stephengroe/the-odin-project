d9ac6343d304a814b840ed8d31c14845f56ba9e2
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2023-09-21 13:58:42 -0700

    Add extra credit features

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -7,7 +7,6 @@ function createLinkedList(name) {
       // Empty and refill wrapper element
       const container = document.querySelector(`.${this.name.replaceAll(" ","-")}`);
 
-      console.log(container.firstChild);
       while (container.firstChild) {
         container.removeChild(container.firstChild);
       }
@@ -15,49 +14,172 @@ function createLinkedList(name) {
       const name = document.createElement("h2");
       name.textContent = this.name;
 
+      const data = document.createElement("ul");
+
+      const size = document.createElement("li");
+      size.textContent = `Size: ${this.getSize()}`;
+
+      const head = document.createElement("li");
+      head.textContent = `Head: ${this.getHead()}`;
+
+      const tail = document.createElement("li");
+      tail.textContent = `Tail: ${this.getTail()}`;
+
+      const index = document.createElement("li");
+      const randomIndex = Math.round(Math.random() * (this.getSize() - 1));
+      index.textContent = `At index [${randomIndex}]: ${this.getItem(randomIndex)}`;
+
+      const contains = document.createElement("li");
+      const randomNumber = Math.round(Math.random() * 10000).toString();
+      contains.textContent = this.contains(randomNumber);
+      
+      data.append(size, head, tail, index, contains);
+
       const prependButton = document.createElement("button");
       prependButton.textContent = "Prepend new node";
       prependButton.addEventListener("click", () => {
-        this.prependNode(Date.now());
+        this.prependNode(Date.now().toString().slice(-4));
         this.render();
-        console.log(this);
+      })
+
+      const appendAtButton = document.createElement("button");
+      appendAtButton.textContent = `Append node at [${randomIndex}]`;
+      appendAtButton.addEventListener("click", () => {
+        this.appendNodeAt(Date.now().toString().slice(-4), randomIndex);
+        this.render();
       })
 
       const appendButton = document.createElement("button");
       appendButton.textContent = "Append new node";
       appendButton.addEventListener("click", () => {
-        this.appendNode(Date.now());
+        this.appendNode(Date.now().toString().slice(-4));
         this.render();
-        console.log(this);
       })
 
-      container.append(name, prependButton, appendButton);
+      const popButton = document.createElement("button");
+      popButton.textContent = "Pop last node";
+      popButton.addEventListener("click", () => {
+        this.popNode();
+        this.render();
+      })
+
+      const removeAtButton = document.createElement("button");
+      removeAtButton.textContent = `Remove node at [${randomIndex}]`;
+      removeAtButton.addEventListener("click", () => {
+        this.removeNodeAt(randomIndex);
+        this.render();
+      })
+
+      container.append(name, data, prependButton, appendAtButton, appendButton, popButton, removeAtButton);
 
       // Add all nodes
       let next = this.head;
+      let counter = 0;
       while (next !== null) {
         const childNode = document.createElement("div");
         childNode.setAttribute("class", "child-node");
-        childNode.textContent = next.value;
+        childNode.textContent = `[${counter}] ${next.value}`;
+
         container.append(childNode);
-
-        console.log(childNode);
-
         next = next.nextNode;
+        counter += 1;
       };
     },
 
     prependNode(value = null) {
       const newNode = createNode(value, this.head);
       this.head = newNode;
-      console.log("Prepended:" + newNode.value + " Next: " + newNode.nextNode);
-      return newNode;
     },
 
-    appendNode(value) {
-      console.log("Appended!");
-      return createNode(value, null);
-    }
+    appendNode(value = null) {
+      const newNode = createNode(value, null);
+      if (this.head === null) {
+        this.head = newNode;
+      } else {
+        let next = this.head;
+        while (next.nextNode !== null) {
+          next = next.nextNode;
+        }
+        next.nextNode = newNode;
+      }
+    },
+
+    popNode() {
+      let next = this.head;
+      if (next === null) return;
+      while (next.nextNode.nextNode !== null) {
+        next = next.nextNode;
+      }
+      next.nextNode = null;
+    },
+
+    appendNodeAt(value, index) {
+      let next = this.head;
+      for (let i=0; i<index-1; i++) {
+        next = next.nextNode;
+      }
+      const newNode = createNode(value, next.nextNode);
+      next.nextNode = newNode;
+    },
+
+    removeNodeAt(index) {
+      let next = this.head;
+      for (let i=0; i<index-1; i++) {
+        next = next.nextNode;
+      }
+      next.nextNode = next.nextNode.nextNode;
+
+    },
+
+    getSize() {
+      let size = 0;
+      let next = this.head;
+      while (next !== null) {
+        next = next.nextNode;
+        size += 1;
+      }
+      return size;
+    },
+
+    getHead() {
+      if (this.head === null) {
+        return null;
+      } else {
+        return this.head.value;
+      }
+    },
+
+    getTail() {
+      if (this.head === null) return null;
+
+      let next = this.head;
+      while (next.nextNode !== null) {
+        next = next.nextNode;
+      }
+      return next.value;
+    },
+
+    getItem(index) {
+      let next = this.head;
+      for (let i=0; i<index; i++) {
+        next = next.nextNode;
+      }
+      if (next === null) return null;
+      return next.value || null;
+    },
+
+    contains(searchValue) {
+      let index = 0;
+      let next = this.head;
+      if (next !== null) {
+        while (next !== null) {
+          if (next.value === searchValue) return `Contains ${searchValue} at [${index}]`;
+          next = next.nextNode;
+          index += 1;
+        }
+      }
+      return `Doesn't contain ${searchValue}`
+    },
   }
 }
 
@@ -78,8 +200,6 @@ document.querySelector("#new-list-button").addEventListener("click", () => {
 
   const newList = createLinkedList(name);
   lists.push(newList);
-
-  console.log(lists);
 
   const div = document.createElement("div");
   div.setAttribute("class", `list-object ${name.replaceAll(" ","-")}`);
